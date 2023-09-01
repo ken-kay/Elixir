2397f8d364b5fa2282181e6507f5a08ae3c7f61a
kenkay <kenanikay@gmail.com>
2023-09-01 21:54:24 +0300

    learning concat

--- hello/hello.exs
+++ hello/hello.exs
@@ -1,5 +1,15 @@
 defmodule Hello do
 
+  def world do
+    IO.puts("Hello World!")
+  end
+
+  def countdown(0), do: 0
+    def countdown(start) do
+      IO.puts(start)
+      countdown(start-1)
+  end
+
   def spin(n) do
     n
     |> Integer.to_string()
@@ -14,5 +24,21 @@ defmodule Hello do
     new_stay = stay * 10 + rem(num,10)
     limbs(new_num, new_stay)
   end
+
+  def maths([]), do: nil
+  def maths([head | tail]) do
+    IO.puts(head)
+    maths(tail)
+  end
+
+  @spec reverse(list[any()], [any()]) :: [any()]
+  def reverse(num, pool \\ [])
+  def reverse([], pool), do: pool
+  def reverse([h | t], pool), do: reverse(t, [h | pool])
+
+  # anon function double = fn x -> x*2 end
+  def concat(from, dest), do: concat_fn(from |> reverse(), dest)
+  def concat_fn([], dest), do: dest
+  def concat_fn([h | t], dest), do: concat_fn(t, [h | dest])
 
 end
