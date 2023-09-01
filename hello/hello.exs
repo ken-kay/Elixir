b78432249175b746e5956862fd62992df2ae5ca5
kenkay <kenanikay@gmail.com>
2023-09-01 18:03:48 +0300

    2 ways to reverse strings

--- hello/hello.exs
+++ hello/hello.exs
@@ -1,12 +1,18 @@
 defmodule Hello do
-  def maths([]), do: nil
-  def maths([head | tail]) do
-    IO.puts(head)
-    maths(tail)
+
+  def spin(n) do
+    n
+    |> Integer.to_string()
+    |> String.reverse()
+    |> String.to_integer()
   end
 
-  def maths_list() do
-    ["Paper", "Pen", "Eraser"]
+  def limbs(num, stay \\ 0)
+  def limbs(0, stay), do: stay
+  def limbs(num, stay) do
+    new_num = div(num,10)
+    new_stay = stay * 10 + rem(num,10)
+    limbs(new_num, new_stay)
   end
 
 end
